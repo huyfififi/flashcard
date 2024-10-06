c1cc2c735810b6d7ea46d975eaaafe8cb5f3da2b
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-10-06 00:06:38 -0700

    :sparkles: Add a function to load JSON flashcards

--- src/flashcard/main.py
+++ src/flashcard/main.py
@@ -1,5 +1,20 @@
+import json
+
 from flashcard.models import FlashCard
 
 
+FLASHCARD_QUESTIONS_PATH = "src/resources/civics_100q.json"
+
+
+def load_flashcards(file_path: str) -> tuple[FlashCard]:
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    return tuple(FlashCard(**flashcard) for flashcard in data)
+
+
 def main():
-    print("Hello, world!")
+    flashcards: tuple[FlashCard] = load_flashcards(FLASHCARD_QUESTIONS_PATH)
+    for flashcard in flashcards:
+        print(f"Question: {flashcard.question}")
+        print(f"Answer: {flashcard.answer}")
+        print()
